c13654804b5094281e6705cb4f198ba47fe6988b
Jesse Ontiveros <locodante@gmail.com>
2018-01-22 14:10:47 -0600

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -60,16 +60,12 @@ app.delete('/todos/:id', (req, res) => {
      if (!todo) {
        return res.status(404).send();
      }
-     res.send(todo);
+     res.send({todo});
    }).catch((e) => {
      res.status(400).send();
    });
 
-
-
-
 });
-
 
 app.listen(port, () => {
   console.log(`Started at port ${port}`);

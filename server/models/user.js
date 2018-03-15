fd73a19dfdeb18f3cbb5195d3645465abc062893
Jesse Ontiveros <locodante@gmail.com>
2018-03-15 14:52:51 -0500

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -51,6 +51,25 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    // the following code is the same as the actual return
+    // return new Promise((resolve, reject) => {
+    //   reject();
+    // });
+    return Promise.reject();
+  }
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 
 
@@ -105,6 +106,25 @@ app.post('/users', (req, res) => {
    }).catch((e) => {
      res.status(400).send(e);
    })
+});
+
+
+
+
+//private route for users
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+  // handled by authenticate above
+  // var token = req.header('x-auth');
+  //
+  // User.findByToken(token).then((user) => {
+  //   if (!user) {
+  //     return Promise.reject();
+  //   }
+  //   res.send(user);
+  // }).catch((e) => {
+  //   res.status(401).send();
+  // });
 });
 
 app.listen(port, () => {
